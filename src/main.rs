1c359d2ab186dc2020aad0f428d338f3d5574fa3
alex <javafirst17@gmail.com>
2022-04-01 23:57:35 +0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{Write};
+use curl::easy::Easy;
 
 struct Dependency {
     repo: String,
@@ -22,9 +23,7 @@ impl Dependency {
     }
 }
 
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let maven_repo = "https://repo1.maven.org/maven2";
 
     let dependencies = vec![Dependency {
@@ -33,16 +32,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         artifact_id: "autocomplete".to_string(),
         version: "3.1.2".to_string(),
     }];
-
     for dep in &dependencies {
-        let resp = reqwest::get(dep.get_url())
-            .await?
-            .bytes()
-            .await?;
+        let mut data = Vec::new();
+        let mut handle = Easy::new();
+        handle.url(&dep.get_url()).unwrap();
+        {
+            let mut transfer = handle.transfer();
+            transfer.write_function(|new_data| {
+                data.extend_from_slice(new_data);
+                Ok(new_data.len())
+            }).unwrap();
+            transfer.perform().unwrap();
+        }
 
-        File::create(dep.get_file_name())?
-            .write_all(&resp).unwrap();
+        File::create(dep.get_file_name()).unwrap()
+            .write_all(&data).unwrap();
     }
-
-    Ok(())
 }
